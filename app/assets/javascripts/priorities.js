deeb571d9dfd4345e5a9acd9db107d05b44013df
cySheng <cyusheng93@gmail.com>
2017-05-05 16:37:06 +0800

    Fixed various css issues and also added new attributes to key in when signing up

--- app/assets/javascripts/priorities.js
+++ app/assets/javascripts/priorities.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+	console.log("hi");
+    var burger = document.querySelector('.nav-toggle');
+    var menu = document.querySelector('.nav-menu');
+    burger.addEventListener('click', function() {
+        burger.classList.toggle('is-active');
+        menu.classList.toggle('is-active');
+    });
+});
