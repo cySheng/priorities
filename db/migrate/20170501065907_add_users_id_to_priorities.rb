9dbd7ef1c3b4e197d0347b206233a7cc578d19da
cySheng <cyusheng93@gmail.com>
2017-05-07 11:07:27 +0800

    Added and migrated votes table

--- db/migrate/20170501065907_add_users_id_to_priorities.rb
+++ db/migrate/20170501065907_add_users_id_to_priorities.rb
@@ -1,5 +1,5 @@
 class AddUsersIdToPriorities < ActiveRecord::Migration[5.0]
   def change
-  	add_reference :priorities, :user, index: true
+  	add_reference :priorities, :user, index: true, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501065907) do
+ActiveRecord::Schema.define(version: 20170507030029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,15 +25,28 @@ ActiveRecord::Schema.define(version: 20170501065907) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "nickname",                       null: false
-    t.string   "email",                          null: false
-    t.string   "encrypted_password", limit: 128, null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
+    t.string   "nickname",                                   null: false
+    t.string   "email",                                      null: false
+    t.string   "encrypted_password", limit: 128,             null: false
     t.string   "confirmation_token", limit: 128
-    t.string   "remember_token",     limit: 128, null: false
+    t.string   "remember_token",     limit: 128,             null: false
+    t.integer  "role",                           default: 0
     t.index ["email"], name: "index_users_on_email", using: :btree
     t.index ["remember_token"], name: "index_users_on_remember_token", using: :btree
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "priority_id"
+    t.integer  "role",        default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.index ["priority_id"], name: "index_votes_on_priority_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
+  end
+
+  add_foreign_key "votes", "priorities"
+  add_foreign_key "votes", "users"
 end
