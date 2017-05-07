79231f19fb98d8f2cc7466c8e531e2f0e89ed566
cySheng <cyusheng93@gmail.com>
2017-05-07 13:14:10 +0800

    Added private params and also create action for prority controller

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -6,4 +6,20 @@ class PrioritiesController < ApplicationController
 	def new
 		@priority = Priority.new
 	end
+
+	def create
+		@priority = current_user.priorities.new(priority_params)
+		if @priority.save
+			flash[:success] = "New priority created"
+		else
+			flash[:error] = "There was a problem creating your priority"
+		end
+		redirect_to root_path
+	end
+
+	private
+
+	def priority_params
+		params.require(:priority).permit(:content, :title)
+	end
 end
