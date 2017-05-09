888017e5b0547491c2e823ce3d228aaaad20a5da
cySheng <cyusheng93@gmail.com>
2017-05-09 17:23:12 +0800

    Added validations for user and sorted our more css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < Clearance::UsersController
 
 	def create
 		@user = User.new(user_params)
-
 		if @user.save 
 			flash[:success] = "Your account has been created"
 			sign_in @user
