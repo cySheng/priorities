3d7638ab2451657cd966c2689b9f6f66b6849a31
cySheng <cyusheng93@gmail.com>
2017-05-09 17:46:44 +0800

    Generated migration file for confirmation email to users. added additional configs into clearance initializer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < Clearance::UsersController
 
 	def create
 		@user = User.new(user_params)
+		@user.email_confirmation_token = Clearance::Token.new
 		if @user.save 
-			flash[:success] = "Your account has been created"
-			sign_in @user
+			flash[:success] = "Your account has been created. You will be sent an email to confirm your account!"
+			UserMailer.registration_confirmation(@user).deliver_later
+      redirect_back_or url_after_create
 		else
 			flash[:error] = "There was an error in creating your account"
 		end
--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,6 @@
 Clearance.configure do |config|
   config.routes = false
-  config.mailer_sender = "reply@example.com"
+  config.mailer_sender = "priorities@example.com"
+   config.sign_in_guards = [ConfirmedUserGuard]
   config.rotate_csrf_on_sign_in = true
 end
