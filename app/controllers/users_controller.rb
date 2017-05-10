a1fec85c79e658194fa12a40603804c27982a4ea
cySheng <cyusheng93@gmail.com>
2017-05-10 13:22:08 +0800

    Added spcific mailer configs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < Clearance::UsersController
 		@user.email_confirmation_token = Clearance::Token.new
 		if @user.save 
 			flash[:success] = "Your account has been created. You will be sent an email to confirm your account!"
-			UserMailer.registration_confirmation(@user).deliver_later
+			UserMailer.registration_confirmation(@user).deliver_now
       redirect_back_or url_after_create
 		else
 			flash[:error] = "There was an error in creating your account"
@@ -13,7 +13,7 @@ class UsersController < Clearance::UsersController
 		redirect_to root_path
 	end
 
-	
+
 
 	private
 
