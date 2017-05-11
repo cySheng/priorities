3d6a6aac7496f4df26f201e6fca3a644c29c931e
cySheng <cyusheng93@gmail.com>
2017-05-11 22:56:09 +0800

    Wrote some comment

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,3 +2,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
+
+#list of tables or timeline list that scrolls up and down and updated with aajx
