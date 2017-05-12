5452142d8cc93d359256d77e307046de0f146e95
cySheng <cyusheng93@gmail.com>
2017-05-12 18:45:27 +0800

    Sorted the votes by highest to lowest order

--- app/controllers/priorities_controller.rb
+++ app/controllers/priorities_controller.rb
@@ -1,9 +1,12 @@
 class PrioritiesController < ApplicationController
 	def index
 		all_of_it = Priority.all
-		@priorities = Priority.all
-
-
+		@priorities = {}
+		byebug
+		all_of_it.each do |x|
+			@priorities[x] = x.get_upvotes.size.to_i - x.get_downvotes.size.to_i
+		end
+		@priorities = @priorities.sort_by {|k,v| v}.reverse
 	end
 
 	def new
